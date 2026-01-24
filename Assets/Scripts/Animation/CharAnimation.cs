c1490e0935d53e0dd1e4ef7a4604288eab91a429
Patipong123 <patipong.saet@bumail.net>
2026-01-24 09:54:10 +0700

    CharacterPrefabs

---
+++ Assets/Scripts/Animation/CharAnimation.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class CharAnimation : MonoBehaviour
+{
+    private Characters character;
+
+    private void Start()
+    {
+        character = GetComponent<Characters>();
+    }
+
+    private void Update()
+    {
+        ChooseAnimation(character);
+    }
+
+    private void ChooseAnimation(Characters c) 
+    {
+        c.Anim.SetBool("IsIdle", false);
+        c.Anim.SetBool("IsWalk", false);
+
+        switch (c.State) 
+        {
+            case CharState.Idle:
+                c.Anim.SetBool("IsIdle", true);
+                break;
+            case CharState.Walk:
+                c.Anim.SetBool("IsWalk", true);
+                break;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Character/CameraController.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    [Header("Move")]
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private Transform corner1;
+    [SerializeField] private Transform corner2;
+
+    [SerializeField] private float xInput;
+    [SerializeField] private float zInput;
+
+    public static CameraController instance;
+
+    [Header("Zoom")]
+    [SerializeField] private float zoomModifier;
+
+    void Awake()
+    {
+        instance = this;
+        cam = Camera.main;
+    }
+
+    void Start()
+    {
+        moveSpeed = 50;
+    }
+
+    void Update()
+    {
+        MoveByKB();
+        Zoom();
+    }
+
+    private void MoveByKB()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
+        transform.position = Clamp(corner1.position, corner2.position);
+    }
+
+    private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
+    {
+        Vector3 pos = new Vector3(Mathf.Clamp(transform.position.x, lowerLeft.x, topRight.x),
+                                  transform.position.y,
+                                  Mathf.Clamp(transform.position.z, lowerLeft.z, topRight.z));
+
+        return pos;
+    }
+
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+            zoomModifier = -0.1f;
+        if (Input.GetKey(KeyCode.X))
+            zoomModifier = 0.1f;
+
+        cam.orthographicSize += zoomModifier;
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 4, 10);
+    }
+}
